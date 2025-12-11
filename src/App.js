999f3734c049ff7da068f405ba94ae9d0c9c4c66
Vikum Manchanayake <vikumviraj@vizuamatix.com>
2025-12-11 09:51:22 +0530

    view todo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
+import Home from './pages/Home';
 
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline font-serif">
-      Hello world!
-    </h1>
-  )
+  return <Home />;
 }
---
+++ src/components/ViewTodo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewTodo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ViewTodo
